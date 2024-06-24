5de90709bfa9c7ca69d852ea0225beb0808a0ca1
MASAru0115 <masaru.matsuda@mail.toyota-body.co.jp>
2024-06-24 18:08:00 +0900

    ssl設定追加

--- backend/src/db/knexfile.js
+++ backend/src/db/knexfile.js
@@ -23,7 +23,14 @@ module.exports = {
   },
   production: {
     client: "postgresql",
-    connection: process.env.DATABASE_URL,
+    connection: {
+      connectString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+        require: true,
+      },
+    },
+
     migrations: {
       directory: "./data/migrations",
     },
