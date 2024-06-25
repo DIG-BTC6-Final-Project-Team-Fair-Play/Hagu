e133dea4f93da8600321322cf54dc46e63038bfe
NISHIZUKA Yoshiki <116330146+YoshikiNishizuka@users.noreply.github.com>
2024-06-25 19:55:22 +0900

    途中ですがdeploy解消も含めてmergeします

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,13 @@
-import { useEffect, useState } from "react";
 import "./App.css";
-import axios from "axios";
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
 import { CreatePage } from "./pages/CreatePage";
 import { SignIn } from "./pages/SignIn";
 
 function App() {
-  const [data, setData] = useState();
-  const sampleGet = async () => {
-    await axios.get("/api/data").then((res) => setData(res.data));
-  };
-  useEffect(() => {
-    sampleGet();
-  }, []);
   return (
     <>
       <div>Hello</div>
-      {JSON.stringify(data)}
       <HomePage></HomePage>
       <CreatePage></CreatePage>
       <SignIn></SignIn>
