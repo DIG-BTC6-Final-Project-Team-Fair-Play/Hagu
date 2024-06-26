a83a66663f028b3e5912fe112bf3e6ee13d57579
otokazu.okumura <otokazu.okumura@toyota.global>
2024-06-26 16:39:19 +0900

    エンドポイントおおよそ実装できました

--- backend/src/db/data/migrations/20240625062554_create_vegetables_table.ts
+++ backend/src/db/data/migrations/20240625062554_create_vegetables_table.ts
@@ -3,7 +3,10 @@ import type { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable("vegetables", (t) => {
     t.increments("id").primary();
-    t.string("vegetable_name");
+    t.text("image");
+    t.string("label");
+    t.text("description");
+    t.string("content");
     t.string("equipment_list");
   });
 }
