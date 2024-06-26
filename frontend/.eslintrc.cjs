48d780a4262edc5d9f6275c01d5d29da0a6a673a
Zukka(Yoshiki_Nishizuka) <116330146+YoshikiNishizuka@users.noreply.github.com>
2024-06-26 18:23:42 +0900

    Update .eslintrc.cjs

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -17,8 +17,5 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
-    "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": "off",
-    "unused-imports/no-unused-imports": "warn",
   },
 };
