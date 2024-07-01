e484a855e84c61d01232e1ea69614c912c3b417b
MASAru0115 <masaru.matsuda@mail.toyota-body.co.jp>
2024-07-01 23:25:08 +0900

    seedファイル修正

--- backend/src/db/data/seeds/003_friends.ts
+++ backend/src/db/data/seeds/003_friends.ts
@@ -6,11 +6,11 @@ export async function seed(knex: Knex): Promise<void> {
 
   // Inserts seed entries
   await knex("friends").insert([
-    { id: 1, user_id: 1, friend_id: 2 },
-    { id: 2, user_id: 1, friend_id: 3 },
-    { id: 3, user_id: 2, friend_id: 1 },
-    { id: 4, user_id: 2, friend_id: 3 },
-    { id: 5, user_id: 3, friend_id: 1 },
-    { id: 6, user_id: 3, friend_id: 2 },
+    { user_id: 1, friend_id: 2 },
+    { user_id: 1, friend_id: 3 },
+    { user_id: 2, friend_id: 1 },
+    { user_id: 2, friend_id: 3 },
+    { user_id: 3, friend_id: 1 },
+    { user_id: 3, friend_id: 2 },
   ]);
 }
