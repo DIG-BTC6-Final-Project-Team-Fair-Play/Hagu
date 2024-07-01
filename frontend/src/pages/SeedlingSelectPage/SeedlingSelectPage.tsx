ffd36d0034c1f9be8068ffe51f5893905d745020
NISHIZUKA Yoshiki <116330146+YoshikiNishizuka@users.noreply.github.com>
2024-07-01 23:23:35 +0900

    Home画面とseedlingの修正

--- frontend/src/pages/SeedlingSelectPage/SeedlingSelectPage.tsx
+++ frontend/src/pages/SeedlingSelectPage/SeedlingSelectPage.tsx
@@ -1,5 +1,5 @@
 import { FooterIcons } from "../../components/FooterIcons";
-import { Header } from "../../components/Header";
+// import { Header } from "../../components/Header";
 import { SeedlingSelect } from "../../components/SeedSelect";
 import { Box, Flex } from "@mantine/core";
 
@@ -7,9 +7,18 @@ export const SeedlingSelectPage = () => {
   return (
     <>
       <Flex direction="column" style={{ height: "100vh" }}>
-        <Box>
-          <Header />
+        <Box h={`calc(100vh - 60px)`}>
+          {/* <Header /> */}
           <SeedlingSelect />
+        </Box>
+        <Box
+          style={{
+            position: "fixed",
+            bottom: 0,
+            left: 0,
+            width: "100%",
+          }}
+        >
           <FooterIcons />
         </Box>
       </Flex>
