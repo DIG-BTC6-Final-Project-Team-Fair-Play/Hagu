4afa86596e7063ac8b201ade581a48e7cfbc55fe
MASAru0115 <masaru.matsuda@mail.toyota-body.co.jp>
2024-07-02 15:52:05 +0900

    fix:seed修正2

--- backend/src/db/data/seeds/002_user.ts
+++ backend/src/db/data/seeds/002_user.ts
@@ -8,15 +8,15 @@ export async function seed(knex: Knex): Promise<void> {
   await knex("users").insert([
     {
       user_name: "banchan",
-      line_id: "U80a60229dc4d5a8a1a0bff25c5c36504",
+      line_id: "Ua7174e236d6a33fd27747ffc57b5ca78",
       picture:
-        "https://profile.line-scdn.net/0hxfqICQElJ0lMMDPsWDlYHnB1KSQ7HiEBNFRhe2g2enljBjVKcFJuJzk2Li1jBjRLIgNsLTs3Lnxp",
+        "https://profile.line-scdn.net/0hQMGeG-FbDmJ6HCWAq35xNUZZAA8NMggqAipDB1xLVQVUJR5kQ3xFV19OAFoCJE0yTnhEUQsYVgJf",
     },
     {
       user_name: "takuma",
-      line_id: "Ua7174e236d6a33fd27747ffc57b5ca78",
+      line_id: "U80a60229dc4d5a8a1a0bff25c5c36504",
       picture:
-        "https://profile.line-scdn.net/0hQMGeG-FbDmJ6HCWAq35xNUZZAA8NMggqAipDB1xLVQVUJR5kQ3xFV19OAFoCJE0yTnhEUQsYVgJf'",
+        "https://profile.line-scdn.net/0hxfqICQElJ0lMMDPsWDlYHnB1KSQ7HiEBNFRhe2g2enljBjVKcFJuJzk2Li1jBjRLIgNsLTs3Lnxp",
     },
   ]);
 }
