474d8216dd9b51df77130b67802c5ef6d95d10a9
MASAru0115 <masaru.matsuda@mail.toyota-body.co.jp>
2024-07-03 16:03:53 +0900

    水やり画面一時保存

--- frontend/src/components/Watering/Watering.tsx
+++ frontend/src/components/Watering/Watering.tsx
@@ -1,8 +1,17 @@
+import { Image } from "@mantine/core";
 
 export const Watering = () => {
   return (
-    <div className='watering'>
-        水やり画面
+    <div className="watering">
+      水やり画面
+      <Image
+        // src={`./images/0${vegetableId}_icon.png`}
+        src={`./images/01_icon.png`}
+        height={"200"}
+        fit="contain"
+        alt="Norway"
+        bg={"#F2EBD9"}
+      />
     </div>
-  )
-}
+  );
+};
--- frontend/src/pages/WateringPage/WateringPage.tsx
+++ frontend/src/pages/WateringPage/WateringPage.tsx
@@ -3,6 +3,10 @@ import { Watering } from "../../components/Watering";
 import { FooterIcons } from "../../components/FooterIcons";
 
 export const WateringPage = () => {
+  // 現在の苗の写真
+  // 水やりのアイコンボタン（遷移はどうする？）
+  // 水やりをしていないと通知部分が赤く光る
+  // 写真記録のアイコンボタん（カメラ起動 navigate）
   return (
     <>
       <Flex direction="column" bg={"#E2D9C1"} style={{ height: "100vh" }}>
